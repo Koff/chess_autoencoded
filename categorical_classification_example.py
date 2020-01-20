d1bf56959b3817d1c5937419693a17e7143fbeca
Fernando San Martin <f.sm.jorquera@gmail.com>
2020-01-20 15:30:56 -0300

    Reconstructing binary images working with chess positions almost perfectly.

--- categorical_classification_example.py
+++ categorical_classification_example.py
@@ -1,35 +1,39 @@
 import numpy as np
 
-from keras.layers import Input, Dense, Conv2D, UpSampling2D, AveragePooling2D
-from keras.utils import to_categorical
-from keras import backend as K, Sequential, Model
-from keras.losses import mse, binary_crossentropy, categorical_crossentropy
+from keras.layers import Input, Conv2D, UpSampling2D, AveragePooling2D
+from keras import Model
 from keras import optimizers
 import matplotlib.pyplot as plt
 
 
-# Generate dummy data
-data_1 = np.random.randint(2, size=(10000, 256)).reshape((10000, 8, 8, 4)).astype(np.int8)
+# Load data
+all_numerical_positions = np.load('x_data.npy')
 
-train, validate = data_1[:8000], data_1[8000:]
+# Cut data to cut training time
+all_numerical_positions = all_numerical_positions[:int(np.floor(all_numerical_positions.shape[0]/10)), :, :, :]
+
+# Transform integers to 4-bit binary
+m = 4
+all_numerical_positions = (((all_numerical_positions[:, None] & (1 << np.arange(m)))) > 0).astype(int)
+all_numerical_positions = all_numerical_positions.reshape((-1, 8, 8, 4))
 
 # this is our input placeholder
 input_pos = Input(shape=(8, 8, 4,))
 
 # "encoded" is the encoded representation of the input
-encoded = Conv2D(64*4, (3, 3), activation='linear', padding='same')(input_pos)
+encoded = Conv2D(64 * 4, (3, 3), activation='linear', padding='same')(input_pos)
 encoded = AveragePooling2D((2, 2), padding='same')(encoded)
-encoded = Conv2D(32*4, (3, 3), activation='linear', padding='same')(encoded)
+encoded = Conv2D(32 * 4, (3, 3), activation='linear', padding='same')(encoded)
 encoded = AveragePooling2D((2, 2), padding='same')(encoded)
-encoded = Conv2D(16*4, (3, 3), activation='linear', padding='same')(encoded)
+encoded = Conv2D(16 * 4, (3, 3), activation='linear', padding='same')(encoded)
 encoded = AveragePooling2D((2, 2), padding='same')(encoded)
 
 # "decoded" is the lossy reconstruction of the input
-decoded = Conv2D(16*4, (3, 3), activation='linear', padding='same')(encoded)
+decoded = Conv2D(16 * 4, (3, 3), activation='linear', padding='same')(encoded)
 decoded = UpSampling2D((2, 2))(decoded)
-decoded = Conv2D(32*4, (3, 3), activation='linear', padding='same')(decoded)
+decoded = Conv2D(32 * 4, (3, 3), activation='linear', padding='same')(decoded)
 decoded = UpSampling2D((2, 2))(decoded)
-decoded = Conv2D(64*4, (3, 3), activation='linear', padding='same')(decoded)
+decoded = Conv2D(64 * 4, (3, 3), activation='linear', padding='same')(decoded)
 decoded = UpSampling2D((2, 2))(decoded)
 decoded = Conv2D(4, (3, 3), activation='linear', padding='same')(decoded)
 
@@ -41,17 +45,18 @@ opt = optimizers.Adam()
 
 autoencoder.compile(optimizer='adam', loss='mse')
 
-autoencoder.fit(train, train,
-                epochs=5,
-                batch_size=50,
+autoencoder.fit(all_numerical_positions,
+                all_numerical_positions,
+                epochs=2,
+                batch_size=256,
                 )
 pass
 
 fig = plt.figure(figsize=(8, 4))
 fig.add_subplot(2, 1, 1)
-plt.imshow(train[100].reshape((8, 32)), cmap='gray')
+plt.imshow(all_numerical_positions[100].reshape((8, 32)), cmap='gray')
 fig.add_subplot(2, 1, 2)
-predicted = autoencoder.predict(train[100].reshape((1, 8, 8, 4))).reshape((8, 32))
+predicted = autoencoder.predict(all_numerical_positions[100].reshape((1, 8, 8, 4))).reshape((8, 32))
 predictions_labels = np.round(predicted[:, :]).astype(int)
 plt.imshow(predictions_labels, cmap='gray')
 plt.show()
